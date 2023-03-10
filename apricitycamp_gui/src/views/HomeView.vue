7ffc84f662141b38c224b6309c998824b55f5a65
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-10 16:40:40 +0200

    carousel added

--- apricitycamp_gui/src/views/HomeView.vue
+++ apricitycamp_gui/src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
- <h2>Home</h2>
+  <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src=" https://i.postimg.cc/VkWxvbPD/kevin-ianeselli-ebnl-Hkqf-UHY-unsplash.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.postimg.cc/V6JG1JFG/jack-sloop-qel-Ga-L2-OLy-E-unsplash.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.postimg.cc/vTq5mhH5/chris-holder-u-Y2-UIy-O5o5c-unsplash.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.postimg.cc/Gmk97zwz/adam-griffith-e-Dif4-JGh-Yes-unsplash.jpg " class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.postimg.cc/y8b3tjLZ/leon-contreras-Ynd-HL7g-QIJE-unsplash.jpg" class="d-block w-100" alt="...">
+    </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </template>
 
 <script>
