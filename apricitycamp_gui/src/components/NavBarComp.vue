b4c2a1ad4734f60e95ab2755db59613a8d6dd9bc
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-10 15:40:14 +0200

    frontend

---
+++ apricitycamp_gui/src/components/NavBarComp.vue
@@ -0,0 +1,90 @@
+<template>
+    
+    <div class="nav justify-content-start">
+      <img src="https://i.postimg.cc/760tLqm1/Apricity-Camp-logo.jpg" style="width:145px; padding-top:5px;">
+    </div>
+  
+  <ul class="nav justify-content-end">
+      
+    <li class="nav-item">
+      <router-link to="/"> Home
+  
+  </router-link> | 
+      
+    </li>
+    <li class="nav-item">
+      <router-link to="/about"> About
+  
+  </router-link> | 
+      
+    </li>
+    <li class="nav-item">
+      <router-link to="/products"> Purchase Here
+        
+  </router-link> |
+      
+    </li>
+    
+    <li class="nav-item">
+      <router-link to="/contact"> Contact
+        
+  </router-link> |
+      
+    </li>
+    
+  </ul>
+  
+  
+  </template>
+  
+  
+  
+  <script>
+  
+  </script>
+  
+  
+  
+  
+  <style scoped>
+  
+  * {
+    margin: 0;
+  }
+  
+  .nav-item {
+  
+    margin-bottom: 10px;
+    margin-left: 30px;
+    font-size: 25px;
+    font-weight: bold;
+  }
+  
+.nav a {
+  color: antiquewhite;
+  text-decoration: none;
+  font-family:  Georgia, "Times New Roman", Times, serif;
+  font-size: 20px;
+  color: black
+
+}
+
+.nav-item hover:a {
+  color: black;
+}
+
+.nav {
+  color:antiquewhite;
+}
+
+.nav {
+  background-color: #f9f3c0;
+  height:75px;
+  
+}
+
+img{
+  height: 20vh;
+}
+  
+  </style>
---
+++ apricitycamp_gui/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+    <li class="nav-item">
+      <router-link to="/register"> Register
+        
+  </router-link> |
+    </li>
+    <li class="nav-item">
+      <router-link to="/login"> Login 
+  </router-link> |
+  </li>
+</ul>
+
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
