6b1687df2d52693e5fd437bb3bbd483fb7d3e634
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-13 11:32:19 +0200

    changed carousel

--- apricitycamp_gui/src/views/HomeView.vue
+++ apricitycamp_gui/src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div id="carouselExample" class="carousel slide">
+  <!-- <div class="container" -->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src=" https://i.postimg.cc/VkWxvbPD/kevin-ianeselli-ebnl-Hkqf-UHY-unsplash.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.postimg.cc/Gmk97zwz/adam-griffith-e-Dif4-JGh-Yes-unsplash.jpg " class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="https://i.postimg.cc/V6JG1JFG/jack-sloop-qel-Ga-L2-OLy-E-unsplash.jpg" class="d-block w-100" alt="...">
@@ -11,21 +22,19 @@
       <img src="https://i.postimg.cc/vTq5mhH5/chris-holder-u-Y2-UIy-O5o5c-unsplash.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://i.postimg.cc/Gmk97zwz/adam-griffith-e-Dif4-JGh-Yes-unsplash.jpg " class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
       <img src="https://i.postimg.cc/y8b3tjLZ/leon-contreras-Ynd-HL7g-QIJE-unsplash.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
 </template>
 
 <script>
@@ -41,4 +50,8 @@ export default {
 </script>
 
 <style scoped>
+img{
+  height: 100vh
+
+}
 </style>
