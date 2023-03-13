fb4c7276a0a1be7da5c22ee4be30078ce6d103f5
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-13 16:40:53 +0200

    view more button links to single page

--- apricitycamp_gui/src/views/SingleProductsView.vue
+++ apricitycamp_gui/src/views/SingleProductsView.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
+   <div class="card" 
+   v-for="product in products"
+   :key="product.id"
+   style="width: 18rem;">
+  <img
+  :src="product.imgURL" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Description:{{ product.prodDescription }}</p>
+    <p class="card-text">Category:{{ product.category }}</p>
+    <p class="card-text">Quantity:{{ product.prodQuantity }}</p>
+  </div>
+</div>
 
-    </div>
+
 </template>
 
 <script>
-    export default {
-        
-    }
+    import { computed } from "@vue/runtime-core";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    store.dispatch("fetchProducts");
+    const products = computed(() => store.state.products);
+    return {
+      products,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
