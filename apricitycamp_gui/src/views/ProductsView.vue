983107ea88a157b40769f4c334b6c6a162974579
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-13 11:34:00 +0200

    retrieving products from database

--- apricitycamp_gui/src/views/ProductsView.vue
+++ apricitycamp_gui/src/views/ProductsView.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        <h2>Products</h2>
+  <div>
+    <h2>Products</h2>
+
+    <div class="products-page">
+      <h1>Browse through our Products:</h1>
+      <div class="row">
+        <div
+          class="card col-3"
+          v-for="product in products"
+          :key="product.id"
+          style="height: 350px"
+        >
+          <img
+            :src="product.imgURL"
+            class="card-img-top"
+            alt="..."
+            style="height: 200px"
+          />
+          <div class="card-body">
+            <p class="name">{{ product.prodName }}</p>
+            <p class="price">R{{ product.price }}</p>
+            <!-- <RouterLink> -->
+
+            <button class="view" href="#">View Details</button>
+            <!-- </RouterLink> -->
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { computed } from "@vue/runtime-core";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    store.dispatch("fetchProducts");
+    const products = computed(() => store.state.products);
+    return {
+      products,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
