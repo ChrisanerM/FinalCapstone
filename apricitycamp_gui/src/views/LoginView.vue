267b84c9da7fdf4a0a583195bd11180ff3037b28
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-14 16:45:41 +0200

    commit

--- apricitycamp_gui/src/views/LoginView.vue
+++ apricitycamp_gui/src/views/LoginView.vue
@@ -1,25 +1,25 @@
 <template>
-    <ul>
+  <ul>
     <li class="nav-item">
-      <router-link to="/register"> Register
-        
-  </router-link> |
+      <router-link to="/register"> Register </router-link> |
     </li>
-    <li class="nav-item">
-      <router-link to="/login"> Login 
-  </router-link> |
-  </li>
-</ul>
-
-    
+    <li class="nav-item"><router-link to="/login"> Login </router-link> |</li>
+  </ul>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { computed } from "@vue/runtime-core";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    store.dispatch("createUser");
+    const users = computed(() => store.state.users);
+    return {
+      users,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
