ef9129cb44bcd662fef81aadee3869de6c8053a8
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-15 16:44:00 +0200

    login created

--- apricitycamp_gui/src/views/LoginView.vue
+++ apricitycamp_gui/src/views/LoginView.vue
@@ -1,23 +1,58 @@
 <template>
-  <ul>
-    <li class="nav-item">
-      <router-link to="/register"> Register </router-link> |
-    </li>
-    <li class="nav-item"><router-link to="/login"> Login </router-link> |</li>
-  </ul>
+  <form @submit.prevent="login">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label"
+        >Email address:</label
+      >
+      <input
+        v-model="emailAdd"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+    <div class="form-floating">
+      <input
+        v-model="userPass"
+        type="password"
+        class="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+      />
+      <label for="floatingPassword">Password</label>
+    </div>
+    <div>
+      <p class="lead">{{ message }}</p>
+    </div>
+    <div class="col-12">
+      <button class="btn btn-primary" type="submit">Click here to login</button>
+    </div>
+  </form>
 </template>
 
 <script>
-import { computed } from "@vue/runtime-core";
-import { useStore } from "vuex";
 export default {
-  setup() {
-    const store = useStore();
-    store.dispatch("createUser");
-    const users = computed(() => store.state.users);
+  data() {
     return {
-      users,
+      emailAdd: "",
+      userPass: "",
     };
+  },
+  computed: {
+    message() {
+      return this.$store.state.message;
+    },
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", {
+        emailAdd: this.emailAdd,
+        userPass: this.userPass,
+      });
+    },
   },
 };
 </script>
