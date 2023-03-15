a3059ce4fa08f801c965cd16b84f2b3e86c687c3
ChrisanerM <chrisanermatthys04@gmail.com>
2023-03-15 16:42:26 +0200

    login and register action added

--- apricitycamp_gui/src/store/index.js
+++ apricitycamp_gui/src/store/index.js
@@ -17,11 +17,17 @@ export default createStore({
     setUsers(state,values){
       state.users=values
     },
+    setUser(state, value) {
+      state.user = value
+    },
     setProducts(state,values){
       state.products=values
     },
+    setProduct(state,value){
+      state.product=value
+    },
     setMessage(state,message){
-      state.message=message
+      state.message= message
     }
     
   },
@@ -43,7 +49,15 @@ export default createStore({
         context.commit('setMessage',err)
       }
   },
-  
+  async fetchProduct(context, id){
+    const res= await axios.get(`${apricityURL}product/${id}`);
+    const {results,err}= await res.data;
+    if (results){
+      context.commit('setProduct',results[0])
+    } else {
+      context.commit('setMessage',err)
+    }
+}, 
   async createUser (context,payload){
     const res= await axios.post(`${apricityURL}register`,payload);
     const {msg,err} = await res.data;
@@ -53,6 +67,16 @@ export default createStore({
       context.commit('setMessage', err)
     }
 },
+  async login (context,payload){
+    const res= await axios.post(`${apricityURL}login`,payload);
+    const {msg,err} = await res.data;
+    if (msg){
+      context.commit('setMessage',msg)
+    } else {
+      context.commit('setMessage', err)
+    }
+},
+ 
     
 
 },
